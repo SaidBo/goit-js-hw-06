48b24bb1fbd084c72a93c46870a39d374a3de29d
SaidBo <bsblfc@gmail.com>
2023-01-17 21:43:04 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const newItem = document.createElement("li");
+  newItem.classList.add("item");
+  newItem.textContent = ingredient;
+  return newItem;
+});
+
+list.append(...elements);
