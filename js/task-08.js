421042a1fa28dec69519dedbb3d9469d01e03094
SaidBo <bsblfc@gmail.com>
2023-01-22 15:28:23 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Заповніть, будь ласка, всі поля");
+  }
+
+  console.log(`Email: ${email.value}, password: ${password.value}`);
+
+  event.currentTarget.reset();
+
+  //   const formData = new FormData(event.currentTarget);
+  //   formData.forEach((value, name) => {
+  //     console.log(name, value);
+  //   })
+}
