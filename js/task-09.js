e83baa3b61aab0ef9d389008b12e8754e2253da9
SaidBo <bsblfc@gmail.com>
2023-01-23 10:48:42 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const spanText = document.querySelector(".color");
+
+buttonEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  spanText.textContent = bodyEl.style.backgroundColor;
+}
