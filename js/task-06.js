b3f1f938de6a700314ad8e719b91dc90389e3afa
SaidBo <bsblfc@gmail.com>
2023-01-25 13:11:46 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,12 +5,13 @@ const refs = {
 const inputLength = Number(refs.input.dataset.length);
 
 refs.input.addEventListener("blur", onInputBlur);
-refs.input.classList.add("invalid");
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.trim().length === inputLength) {
-    refs.input.classList.replace("invalid", "valid");
+  if (event.target.value.trim().length === inputLength) {
+    refs.input.classList.add("valid");
+    refs.input.classList.remove("invalid");
   } else {
-    refs.input.classList.replace("valid", "invalid");
+    refs.input.classList.remove("valid");
+    refs.input.classList.add("invalid");
   }
 }
